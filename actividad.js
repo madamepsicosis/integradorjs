49eab3e4f96ef2ed93c3afcee17d406a812d1b30
vero <veroo.corvalan@gmail.com>
2022-10-24 20:58:17 -0300

    nuevo

--- actividad.js
+++ actividad.js
@@ -7,14 +7,15 @@ function calcTotal(){
     let cantidad=document.getElementById("cantidad").value
     let categoria=document.getElementById("categoria").value
 
-    let total=0
+   
 
-    if (categoria==1){
-        total = ticket*cantidad*.2
-    } else if (categoria==2){
-        total = ticket*cantidad*.5
-    } else (categoria==3);{
-        total = ticket*cantidad*.85
+    let total=0
+    if (categoria==1) {
+        total=cantidad*ticket*.2 
+    } else if (categoria==2) {
+        total=cantidad*ticket*.5 
+    } else {
+        total=cantidad*ticket*.85 
     }
 
     document.getElementById("total").innerHTML=`Total a pagar: $${total}`
