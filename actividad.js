143283e6a45b054cd7346e02b3854e5ad1ef320a
Verónica Corvalán <98621746+madamepsicosis@users.noreply.github.com>
2022-10-24 21:05:16 -0300

    Update actividad.js

--- actividad.js
+++ actividad.js
@@ -8,14 +8,13 @@ function calcTotal(){
     let categoria=document.getElementById("categoria").value
 
    
-
     let total=0
     if (categoria==1) {
-        total=cantidad*ticket*.2 //80%
+        total=cantidad*ticket*.2 
     } else if (categoria==2) {
-        total=cantidad*ticket*.5 //50%
+        total=cantidad*ticket*.5 
     } else {
-        total=cantidad*ticket*.85 //15%
+        total=cantidad*ticket*.85 
     }
 
     document.getElementById("total").innerHTML=`Total a pagar: $${total}`
